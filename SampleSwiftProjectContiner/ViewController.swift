b9e55ac7cd96becb7afb035f3b26bcb7d3bdb76d
Sunilkumarr <sunil@applozic.com>
2021-03-08 12:45:38 +0530

    Initial Commit

---
+++ SampleSwiftProjectContiner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleSwiftProjectContiner
+//
+//  Created by apple on 08/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
